08cbc60d28ae6c1329387b78d4b4c681d3781155
Ankit <ankit.sharma@utexas.edu>
2016-04-29 10:51:15 -0500

    Added opencv color groupings and tag helper

---
+++ app/src/main/java/williamamills/colorify/InstagramTagAPIHelper.java
@@ -0,0 +1,137 @@
+package williamamills.colorify;
+
+/**
+ * Created by Ankit on 4/29/16.
+ */
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class InstagramTagAPIHelper extends AsyncTask<Void, Void, String> {
+
+    /* Popular Endpoint*/
+    String API_URL = "https://api.instagram.com/v1/media/popular?client_id=e05c462ebd86446ea48a5af73769b602";
+    String start_tags_url = "https://api.instagram.com/v1/tags/";
+    String end_tags_url = "/media/recent?client_id=e05c462ebd86446ea48a5af73769b602";
+    Context ctx;
+    MainActivity activity;
+    String tag;
+
+    protected void onPreExecute() {
+        /* initialization before network call in background,
+        * potentially do add different endpoints/search criteria */
+
+    }
+
+    public InstagramTagAPIHelper(MainActivity act, Context context, String input_tag){
+        activity = act;
+        ctx = context;
+        tag = input_tag;
+    }
+
+    protected String doInBackground(Void... urls) {
+
+        try {
+            URL url = new URL(start_tags_url + tag + end_tags_url);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        /* what do with the network call response,
+         * potentially to store into sql db */
+        if(response == null) {
+            Toast.makeText(ctx, "THERE WAS AN ERROR RETRIEVING JSON DATA", Toast.LENGTH_LONG).show();
+            System.out.println("THERE WAS AN ERROR RETRIEVING JSON DATA");
+        }
+        else {
+            try{
+                JSONObject json = new JSONObject(response); //contains meta and data
+                JSONArray data = json.getJSONArray("data"); //only get data, ignore meta
+
+                for(int i =0;i<data.length();i++){
+                    System.out.println(data.getJSONObject(i)); // print returned json objects
+                }
+                ArrayList<String> tester = new ArrayList<>();
+                ArrayList<Photo> photoList = new ArrayList<>();
+                for(int i = 0; i < data.length(); i++) {
+                    JSONObject test = data.getJSONObject(i); //photo at index 1
+                    JSONObject images = test.getJSONObject("images");
+                    //JSONObject thumbnail = images.getJSONObject("thumbnail");
+                    //String thumbnailUrl = thumbnail.getString("url");
+                    String highQuality = images.getJSONObject("standard_resolution").getString("url");
+
+                    String caption = "";
+                    if(!test.isNull("caption")){
+                        caption = test.getJSONObject("caption").getString("text");
+                    }
+                    String tags = "";
+                    if(!test.isNull("tags")){
+                        tags = test.getString("tags");
+                    }
+                    String location = "";
+                    if(!test.isNull("location")){
+                        location = test.getString("location");
+                    }
+                    String likes = "";
+                    if(!test.isNull("likes")){
+                        likes = test.getJSONObject("likes").getString("count");
+                    }
+                    Photo photo = new Photo(caption, tags, likes, ctx.getResources().getString(R.string.image_path) + i, location);//location, tags, caption);
+                    //tester.add(thumbnailUrl);
+                    tester.add(highQuality);
+                    photoList.add(photo);
+                }
+                String[] array = tester.toArray(new String[0]);
+                GetBitmap g = new GetBitmap(ctx, photoList, false);
+                try {
+                    g.execute(array);
+                    g.get(1000, TimeUnit.MILLISECONDS);
+                }catch(Exception e){
+
+                }
+
+                System.out.println(data.length()); //print number of objects returned (~24)
+            } catch(JSONException e){
+                System.out.println("JSONERROR: " + e.getMessage());
+            }
+
+        }
+
+    }
+
+}
+
--- app/src/main/java/williamamills/colorify/OpenCVHelper.java
+++ app/src/main/java/williamamills/colorify/OpenCVHelper.java
@@ -2,11 +2,16 @@ package williamamills.colorify;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,35 +25,56 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.TermCriteria;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Created by Ankit on 4/18/2016.
  */
-public class OpenCVHelper extends AsyncTask<String, Void, String> {
+public class OpenCVHelper extends AsyncTask<String, Void, String[]> {
 
     Context ctx;
+
+    public enum myColors {
+        RED, YELLOW, GREEN, CYAN, BLUE, PURPLE
+    }
 
     public OpenCVHelper(Context c) {
         ctx = c;
     }
 
-    protected String doInBackground(String... url){
+    protected String[] doInBackground(String... url){
         Mat img = null;
         try {
-            img = Utils.loadResource(ctx, R.drawable.lena, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+            img = Utils.loadResource(ctx, R.drawable.dog, Imgcodecs.CV_LOAD_IMAGE_COLOR); // Load image
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Reading\n");
-        System.out.println(img.rows());
         int k = 2;
-        Mat clusters = cluster(img, k).get(0);
-        System.out.println(clusters.rows());
-        File file = new File(ctx.getFilesDir(), "test2.png");
-        Imgcodecs.imwrite(file.getPath(),clusters);
-        System.out.print("Writing\n");
+        Mat clusters = cluster(img, k).get(0); // Perform k-means algorithm
 
-        return "Done";
+        Imgproc.cvtColor(clusters, clusters, Imgproc.COLOR_BGR2RGB); // Convert to RGB Color Space
+
+        Bitmap b = Bitmap.createBitmap(clusters.width(), clusters.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(clusters,b); // Write to bitmap
+
+//        File file = new File(ctx.getFilesDir(), "test2.png");
+//        // Imgcodecs.imwrite(file.getPath(), clusters);
+//        System.out.print("Writing\n");
+//
+//        try {
+//            OutputStream outStream = new FileOutputStream(file);
+//            b.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+        String[] colors = getColors(b); // Place colors of image into buckets
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+
+
+        return colors;
     }
     public static List<Mat> cluster(Mat cutout, int k) {
         Mat samples = cutout.reshape(1, cutout.cols() * cutout.rows());
@@ -57,7 +83,6 @@ public class OpenCVHelper extends AsyncTask<String, Void, String> {
         Mat labels = new Mat();
         TermCriteria criteria = new TermCriteria(TermCriteria.COUNT, 100, 1);
         Mat centers = new Mat();
-        System.out.println(cutout.rows());
         Core.kmeans(samples32f, k, labels, criteria, 1, Core.KMEANS_PP_CENTERS, centers);
         return showClusters(cutout, labels, centers);
     }
@@ -83,4 +108,77 @@ public class OpenCVHelper extends AsyncTask<String, Void, String> {
         }
         return clusters;
     }
+
+    private static String[] getColors(Bitmap b) {
+
+        int c1 = b.getPixel(30,30);
+        int c2 = 0;
+
+        next:
+        for (int y = 20; y < b.getHeight()-20; y++)
+        {
+            for (int x = 20; x < b.getWidth()-20; x++) {
+                int tempColor = b.getPixel(x, y);
+                if (tempColor != c1) {
+                    c2 = tempColor;
+                    break next;
+                }
+            }
+        }
+
+        String hexColor1 = String.format("#%06X", (0xFFFFFF & c1));
+        String hexColor2 = String.format("#%06X", (0xFFFFFF & c2));
+
+        System.out.println(c1);
+        System.out.println(hexColor1);
+        System.out.println(c2);
+        System.out.println(hexColor2);
+
+        float[] hsv1 = new float[3];
+        float[] hsv2 = new float[3];
+
+        Color.colorToHSV(c1,hsv1);
+        Color.colorToHSV(c2,hsv2);
+
+        hsv1[1] = (float) 0.5;
+        hsv1[2] = (float) 0.5;
+        hsv2[1] = (float) 0.5;
+        hsv2[2] = (float) 0.5;
+
+        String[] colors = new String[2];
+
+        if(c1 <= -16777200){
+            colors[0] = "BLACK";
+            String s2 = getBucket(hsv2[0]);
+            colors[1] = s2;
+        } else if(c2 <= -16777200){
+            colors[1] = "BLACK";
+            String s1 = getBucket(hsv1[0]);
+            colors[0] = s1;
+        } else {
+            String s1 = getBucket(hsv1[0]);
+            String s2 = getBucket(hsv2[0]);
+            colors[0] = s1;
+            colors[1] = s2;
+        }
+        return colors;
+    }
+
+    private static String getBucket(float hue){
+        if(hue > 320 || hue <= 25){
+            return myColors.RED.name();
+        } else if(hue > 25 && hue <= 70){
+            return myColors.YELLOW.name();
+        } else if(hue > 70 && hue <= 140){
+            return myColors.GREEN.name();
+        } else if(hue > 140 && hue <= 205){
+            return myColors.CYAN.name();
+        } else if(hue > 205 && hue <= 250){
+            return myColors.BLUE.name();
+        } else if(hue > 250 && hue <= 320){
+            return myColors.PURPLE.name();
+        }
+        return myColors.YELLOW.name();
+    }
+
 }
